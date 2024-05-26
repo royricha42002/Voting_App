148cc96699e5fe0eb4351593c071b1fe1b27fb2d
AdityaSingh-02 <asaditya2002@gmail.com>
2024-05-26 12:15:07 +0530

    fix: internal server error

--- routess/userRoutes.js
+++ routess/userRoutes.js
@@ -1,86 +1,66 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./../modelss/user');
-const {jwtAuthMiddleware, generateToken} = require('./../jwt');
+const { jwtAuthMiddleware, generateToken } = require('./../jwt');
 
-// POST route to add a person
-router.post('/signup', async (req, res) =>{
-    try{
-        const data = req.body // Assuming the request body contains the User data
-
-        // Check if there is already an admin user
+router.post('/signup', async (req, res) => {
+    try {
+        const data = req.body
         const adminUser = await User.findOne({ role: 'admin' });
         if (data.role === 'admin' && adminUser) {
             return res.status(400).json({ error: 'Admin user already exists' });
         }
 
-         // Create a new User document using the Mongoose model
         const newUser = new User(data);
+        const response = await newUser.save();
+        console.log('data saved');
 
-         // Save the new user to the database
-         const response = await newUser.save();
-         console.log('data saved');
-
-         //sending just userid incase token gets hack then aadhar info wont be leaked
-         const payload = {
+        const payload = {
             id: response.id
         }
+        const token = await generateToken(payload);
 
-        console.log(JSON.stringify(payload));
-        const token = generateToken(payload);
-
-        console.log(token);
-
-        res.status(200).json({response: response, token: token});
+        res.status(201).json({ token })
     }
-    catch(err){
+    catch (err) {
         console.log(err);
-        res.status(500).json({error: 'Internal Server Error'});
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 })
 
-// Login Route
-router.post('/login', async(req, res) => {
-    try{
-        // Extract aadharCardNumber and password from request body
-        const {aadharCard, password} = req.body;
-
-        // Check if aadharCardNumber or password is missing
+router.post('/login', async (req, res) => {
+    try {
+        const { aadharCard, password } = req.body;
         if (!aadharCard || !password) {
             return res.status(400).json({ error: 'Aadhar Card Number and password are required' });
         }
 
-        // Find the user by aadharCardNumber
-        const user = await User.findOne({aadharCard: aadharCard});
-
-        // If user does not exist or password does not match, return error
-        if( !user || !(await user.comparePassword(password))){
-            return res.status(401).json({error: 'Invalid Aadhar Card Number or Password'});
+        const user = await User.findOne({ aadharCard: aadharCard });
+        if (!user || !(await user.comparePassword(password))) {
+            return res.status(401).json({ error: 'Invalid Aadhar Card Number or Password' });
         }
 
-        // generate Token 
         const payload = {
             id: user.id,
         }
+
         const token = generateToken(payload);
 
-        // resturn token as response
-        res.json({token})
-    }catch(err){
+        res.json({ token })
+    } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
- 
 
-// Profile route
+
 router.get('/profile', jwtAuthMiddleware, async (req, res) => {
-    try{
+    try {
         const userData = req.user;
         const userId = userData.id;
         const user = await User.findById(userId);
-        res.status(200).json({user});
-    }catch(err){
+        res.status(200).json({ user });
+    } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });
     }
